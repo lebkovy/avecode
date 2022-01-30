fc912d7ef44cf61fef7937a72a00ddecffb1384d
lebkovy <Tima2305>
2022-01-30 15:55:26 +0100

    set up template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import {HomeComponent} from "./components/home/home.component";
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'skills', component: SkillsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SkillsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
