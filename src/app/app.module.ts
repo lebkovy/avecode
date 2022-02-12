98a74c01d152b4107687c6be7f25d9fb26b0e302
lebkovy <Tima2305>
2022-02-12 18:16:05 +0100

    set up template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import {HomeComponent} from "./components/home/home.component";
+import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { GridSectionComponent } from './shared/components/grid-section/grid-section.component';
+import { LatestComponent } from './components/latest/latest.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,11 +20,15 @@ export const routes: Routes = [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SkillsComponent
+    SkillsComponent,
+    GridSectionComponent,
+    SkillsComponent,
+    LatestComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
