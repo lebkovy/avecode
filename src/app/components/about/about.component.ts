a774e80085ac028dcddf1b78869356ecdd4a12a6
jakublebiedzinski <jakub.lebiedzinski@onoco.com>
2023-03-05 12:27:00 +0100

    Update

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,12 +9,19 @@ import {IQuote} from "../../shared/model/quote";
 export class AboutComponent implements OnInit {
     quotes: IQuote[] = [
         {
-            name: 'Agnieszka',
+            name: 'Agnes',
             position: 'Product Designer',
             text: 'I had the opportunity to collaborate with Kuba on the Android and iOS app. Kuba is hardworking,\n' +
                 '        engaged in creating the best solutions, he pays great attention to human center design. Working with\n' +
                 '        him was a true pleasure!'
         },
+        {
+            name: 'Margaret',
+            position: 'Founder',
+            text: 'Kuba is a true master of mobile development.\n' +
+                'His talent for creating intuitive and user-friendly apps is second to none,\n' +
+                'and his dedication to his craft is truly inspiring.'
+        }
     ]
     goals: string[] = [
         'clean design', 'optimized code', 'sustainability', 'functionality', 'useful', 'transparency', 'reliability'
