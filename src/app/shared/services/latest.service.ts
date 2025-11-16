1813952226e285ca92ef1e3335b69b3230e2bb22
Jakub Lebiedziński <lebiedzinski.jakub.maciej@gmail.com>
2025-11-16 19:16:19 +0100

    Implement code changes to enhance functionality and improve performance

--- src/app/shared/services/latest.service.ts
+++ src/app/shared/services/latest.service.ts
@@ -1,25 +1,22 @@
 import {Injectable} from '@angular/core';
-import {faHandHoldingUsd, faLandmark, faPiggyBank, faSearchDollar} from "@fortawesome/free-solid-svg-icons";
+import {faFilter, faInfo, faLocationArrow, faSearchLocation} from "@fortawesome/free-solid-svg-icons";
 import {IPill} from "../model/pill";
 
 @Injectable({
     providedIn: 'root'
 })
 export class LatestService {
-    title: string = 'iVault';
-    subtitle: string = 'A mobile app to manage and organize your currency operations.';
-    description: string = 'iValut is an application that allows you to calculate your gains or losses on currency operations. An\n' +
-        '            algorithm compares the current exchange rate of a given currency and calculates the difference of your\n' +
-        '            balance. If the balance is positive, you have gained the calculated amount - if not, you have lost the\n' +
-        '            indicated amount.';
+    title: string = 'Celiapka';
+    subtitle: string = 'A mobile app to discover gluten-free venues and products near you..';
+    description: string = 'Celiapka is an app that allows quick and easy searching for places offering gluten-free meals and products. Thanks to integration with certified databases, you can be sure of the high quality and up-to-date information. The app offers features such as geolocation, filtering results by type of venue, opening hours, distance, and navigation to the chosen place.\n' + 'Celiapka works both online and offline, with personalization based on the user’s location. We also care about your privacy — using the app requires consent to process location data, which is used solely to improve functionality.\n' + 'Join the Celiapka community and enjoy a safe and convenient choice of gluten-free places and products every day!';
     pills: IPill[] = [
-        {label: 'Choose Currency', icon: faSearchDollar},
-        {label: 'Provide Operations', icon: faHandHoldingUsd},
-        {label: 'Store History', icon: faLandmark},
-        {label: 'Monitor Savings', icon: faPiggyBank},
+        {label: 'Search for venue', icon: faSearchLocation},
+        {label: 'Filter places', icon: faFilter},
+        {label: 'Check details', icon: faInfo},
+        {label: 'Navigate to place', icon: faLocationArrow},
     ]
 
     get androidLink() {
-        return 'https://play.google.com/store/apps/details?id=com.lebkovy.'+this.title.toLowerCase();
+        return 'https://play.google.com/store/apps/details?id=com.avecode.'+this.title.toLowerCase();
     }
 }
